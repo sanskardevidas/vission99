6d5410b50c18a5a8f9c8f5aa5b0c5387cc52d93c
sanskardevidas <sanskardevidas@gmail.com>
2026-06-20 11:27:01 +0530

    database updated

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
+console.log('URL:', import.meta.env.VITE_SUPABASE_URL);
+console.log('KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY);
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error(
-    'Missing Supabase environment variables. Check your .env file.'
-  );
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
