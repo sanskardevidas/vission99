6c30584452802dce9023ecf39d85a3b4b41d84bb
Harshu <harshada1918@gmail.com>
2026-07-09 13:58:53 +0530

    changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -60,19 +60,12 @@ useEffect(() => {
     </AnimatePresence>
     {!showIntro && (
      <motion.main
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.8, ease: "easeInOut" }}
-      >
-        <HeroSection />
-        <HowItWorksSection />
-        <CitySelectionSection />
-        <FeaturedExperiencesSection />
-        <VRExperienceSection />
-        <FounderSection />
-        <SuccessStoriesSection />
-        <BookExperienceSection />
-      </motion.main>
+  initial={{ opacity: 0 }}
+  animate={{ opacity: 1 }}
+  transition={{ duration: 0.8, ease: "easeInOut" }}
+>
+  <HeroSection />
+</motion.main>
     )}
   </>
   )};
